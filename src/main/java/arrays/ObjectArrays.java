0ca3d6a58f179473eab241b8432a3d2b3e0f5d3d
Aktilek <nasirovaktilek@gmail.com>
2021-08-11 14:51:58 +0600

    * search by name * phone,email,password regex

--- src/main/java/arrays/ObjectArrays.java
+++ src/main/java/arrays/ObjectArrays.java
@@ -1,20 +1,94 @@
 package arrays;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ObjectArrays {
+    static Product[] products = new Product[]{
+            new Product(1000, "Apple", "iPhone 12 mini"),
+            new Product(1100, "Apple", "iPhone 12"),
+            new Product(1200, "Apple", "iPhone 12 Pro"),
+            new Product(700, "Xiaomi", "Mi 12 Lite"),
+            new Product(800, "Xiaomi", "Mi 12"),
+            new Product(990, "Xiaomi", "Mi 12 Pro"),
+            new Product(800, "Samsung", "A10 a"),
+            new Product(900, "Samsung", "A10 b"),
+            new Product(970, "Samsung", "A10 c"),
+    };
+
     public static void main(String[] args) {
-        Product[] products = new Product[]{
-                new Product(1200, "Apple"),
-                new Product(700, "Xiaomi"),
-                new Product(800, "Samsung"),
-        };
+//        System.out.println("Samsung: " + sumOfProductFor("Samsung"));
+//        System.out.println("Xiaomi: " + sumOfProductFor("Xiaomi"));
+//        System.out.println("Apple: " + sumOfProductFor("Apple"));
+//
+        List<Product> nameProducts = searchByName("M");
+            for (Product pr : nameProducts) {
+                printProductInfo(pr);
+            }
+
+//        List<Product> findProd = searchInPrice(900, 1100);
+//        for (Product pr : findProd) {
+//            printProductInfo(pr);
+//        }
+    }
+
+    private static int sumOfProductFor(String owner) {
+        int a = 0;
+        for (Product pro : products) {
+            if (owner.equals(pro.owner)) {
+                a += pro.price;
+            }
+        }
+        return a;
+    }
+
+    /**
+     * Поиск по совпадению начала имени
+     * @param name
+     * @return
+     */
+    private static List<Product> searchByName(String name) {
+        List<Product> findName = new ArrayList<>();
+        for (Product prod : products) {
+            if (prod.name.startsWith(name)) {
+                findName.add(prod);
+            }
+        }
+        return findName;
+    }
+
+    private static List<Product> searchInPrice(int fromPrice, int toPrice) {//from 900 to 1000
+        List<Product> findProducts = new ArrayList<>();
+        for (Product product : products) {
+            // 990
+            //        900 <= 990           &&    1000 >= 990
+            if (fromPrice <= product.price && toPrice >= product.price){
+                findProducts.add(product);
+            }
+        }
+        return findProducts;
+    }
+
+    private static void printProductInfo(Product product) {
+        if (product != null) {
+            System.out.println("Product name: " + product.name);
+            System.out.println("owner: " + product.owner);
+            System.out.println("price: " + product.price);
+        } else {
+            System.out.println("Product is null");
+        }
     }
 
     static class Product {
         int price;
+        String owner;
         String name;
 
-        Product(int price, String name) {
+        Product(int price, String owner, String name) {
             this.price = price;
+            this.owner = owner;
             this.name = name;
         }
     }
--- src/main/java/regexs/RegexEx.java
+++ src/main/java/regexs/RegexEx.java
@@ -1,21 +1,38 @@
 package regexs;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class RegexEx {
     public static void main(String[] args) {
-        String phoneO = "0707142842";// 07**, 05**, 09**, 02**
-        String email = "email@gmail.com"; // *@{2+}.{2+}
-        String password = "Qwerty123@"; // хотя бы одина большая буква, одна маленькая, одна цифра, один смвол
+        String phone = "0700180085518899";// 07**, 05**, 09**, 02**
+        String email = "emailtest@gmail.com"; // *@{2+}.{2+}
+        String password = "Qwerty123@"; // хотя бы одна большая буква, одна маленькая, одна цифра, один символ
 
-        if(phoneO.matches("regex")){
-            System.out.println("Номер телефона верный");
+        String phoneRegex = "(^[0])([2,7,5,9])([0-9]{8})";
+
+        if (phone.matches(phoneRegex)) {
+            System.out.println("Номер " + phone + " корректный");
+        } else {
+            System.out.println("Номер" + phone + " некорректный");
         }
 
-        if(email.matches("regex")){
-            System.out.println("Емейл верный");
+        String emailRegex = "(\\w{3,})@([a-z]{2,}+\\.)([a-z]{2,4})";
+        Pattern p2 = Pattern.compile(emailRegex);
+        Matcher m2 = p2.matcher(email);
+        if (m2.find()) {
+            System.out.println("e-: " + m2.group() + " правильный");
+        } else {
+            System.out.println("e-mail " + email + " некорректный");
         }
 
-        if(password.matches("regex")){
+        String passwordRegex = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}";
+        Pattern p3 = Pattern.compile(passwordRegex);
+        Matcher m3 = p3.matcher(password);
+        if (password.matches(passwordRegex)) {
             System.out.println("Пароль надёжный");
+        } else {
+            System.out.println("Пароль не надёжный");
         }
     }
 }
